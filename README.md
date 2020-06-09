a4fd2e12eb57793151e311d4d9a2487794072708
angel-villa <65564181+angel-villa@users.noreply.github.com>
2020-06-09 11:08:46 -0500

    generalize knot points

--- README.md
+++ README.md
@@ -4,12 +4,35 @@
  To do: generalize to any number of knots
  
  Noisy tanh(x) from x = -1 to x = 2
+ ------
+ 
+ 5 knot points 
  
  ![](plot_tanh.png)
  
- Noisy sin(2 x^2) from x = -1 to x = 2
+ 10 knot points
  
- Limited by number of knot points
+ ![](plot_tanh_10knots.png)
+ 
+ 100 knot points
+ 
+ ![](plot_tanh_100knots.png)
+ 
+ 
+ Noisy sin(2 x^2) from x = -1 to x = 2. 
+ ------
+ 
+ Limited by number of knot points when using 5. Some overfitting at 10.
+ 
+ 5 knot points
  
  ![](plot_sin_2_xsq.png)
+ 
+ 10 knot points
+ 
+ ![](plot_sin_2_xsq_10knots.png)
+ 
+ 100 knot points
+ 
+ ![](plot_sin_2_xsq_100knots.png)
  
--- cubic_spline.m
+++ cubic_spline.m
@@ -2,35 +2,39 @@
 
 clear; clc;
 
-% rng(1)
+rng(1)
 x = [-1:0.02:2]';
-y_actual = [sin(2*x.^2)];
+y_actual = [tanh(x)];
 y = y_actual + 0.2*randn(length(x),1);
 N = length(x);
 
+num_knots = 100;
+
+% Uncomment the following lines to test various lambda values
+
 % Initialize lambda and EPE vectors with length N_l
-test_lambda = 0:0.5:5;
-N_l = length(test_lambda);
-EPE = zeros(N_l,1);
-
-% Calculate entries for EPE vector
-for i=1:N_l
-    EPE(i) = calc_E_val(x,y,test_lambda(i));
-end
-
-plot(test_lambda',EPE)
-title('Plot of EPE at various values of lambda')
-xlabel('lambda')
-ylabel('EPE')
+% test_lambda = 0:0.5:5;
+% N_l = length(test_lambda);
+% EPE = zeros(N_l,1);
+% 
+% % Calculate entries for EPE vector
+% for i=1:N_l
+%     EPE(i) = calc_E_val(x,y,test_lambda(i),num_knots);
+% end
+% 
+% plot(test_lambda',EPE)
+% title('Plot of EPE at various values of lambda')
+% xlabel('lambda')
+% ylabel('EPE')
 
 % Optimal lambda (lowest EPE) is 0
 % large lambda gives a linear approximation
 lambda = 0;
 
 % Calculate knot points vector xi
-xi = zeros(5,1);
-for j=1:5
-    index = floor(N/6*j);
+xi = zeros(num_knots,1);
+for j=1:num_knots
+    index = floor(N/(num_knots + 1)*j);
     xi(j) = x(index);
 end
 
@@ -41,7 +45,7 @@ Nb = calc_Nb(N,x,xi);
 omega = calc_omega(N,x,xi);
 
 % Calculate theta vector
-theta = (Nb'*Nb + lambda*eye(5)*omega)^-1*Nb'*y;
+theta = (Nb'*Nb + lambda*eye(num_knots)*omega)^-1*Nb'*y;
 
 % Calculate f(X) for optimal lambda
 f = zeros(N,1);
@@ -60,7 +64,7 @@ title('Plot of noisy data y and natural cubic spline spline f')
 xlabel('x')
 
 % Calculate EPE using leave one out cross-validation
-function E = calc_E_val(x,y,lambda)
+function E = calc_E_val(x,y,lambda,num_knots)
     E = 0;
     
     x_orig = x;
@@ -78,9 +82,9 @@ function E = calc_E_val(x,y,lambda)
         y(i) = [];
         
         % Calculate knot points vector xi
-        xi = zeros(5,1);
-        for j=1:5
-            index = floor(N/6*j);
+        xi = zeros(num_knots,1);
+        for j=1:num_knots
+            index = floor(N/(num_knots + 1)*j);
             xi(j) = x(index);
         end
         
@@ -91,7 +95,7 @@ function E = calc_E_val(x,y,lambda)
         omega = calc_omega(N,x,xi);
 
         % Calculate theta vector
-        theta = (Nb'*Nb + lambda*eye(5)*omega)^-1*Nb'*y;
+        theta = (Nb'*Nb + lambda*eye(num_knots)*omega)^-1*Nb'*y;
         
         % Calculate bases N(x) for x(i) left out, for testing error
         Nt = calc_Nb(1,x_orig(i),xi);
@@ -105,24 +109,21 @@ end
 
 % Calculate N basis matrix
 function nb = calc_Nb(N,x,xi)
-    nb = zeros(N,5);
+    num_knots = length(xi);
+    nb = zeros(N,num_knots);
     for i=1:N
-        for j=1:5
-            % Calculate dj(x) for each x(i), i=1:N
-            d1 = (max((x(i)-xi(1))^3,0)-max((x(i)-xi(5))^3,0))/(xi(5)-xi(1));
-            d2 = (max((x(i)-xi(2))^3,0)-max((x(i)-xi(5))^3,0))/(xi(5)-xi(2));
-            d3 = (max((x(i)-xi(3))^3,0)-max((x(i)-xi(5))^3,0))/(xi(5)-xi(3));
-            d4 = (max((x(i)-xi(4))^3,0)-max((x(i)-xi(5))^3,0))/(xi(5)-xi(4));
+        dm = zeros(num_knots - 1, 1);
+        for j=1:num_knots
+            % Calculate dm_j(x) for each x(i), i=1:N
+            for k = 1:num_knots - 1
+                dm(k) = (max((x(i)-xi(k))^3,0)-max((x(i)-xi(num_knots))^3,0))/(xi(num_knots)-xi(k));
+            end
             if j==1
                 nb(i,j) = 1;
             elseif j==2
                 nb(i,j) = x(i);
-            elseif j==3
-                nb(i,j) = d1-d4;
-            elseif j==4
-                nb(i,j) = d2-d4;
-            elseif j==5
-                nb(i,j) = d3-d4;
+            elseif j>2
+                nb(i,j) = dm(j - 2) - dm(num_knots - 1);
             end
         end
     end
@@ -130,21 +131,23 @@ end
     
 % Calculate N''i(x) to integrate for omega calculation
 function Npp = calc_Npp(i,j,x,xi)
+    num_knots = length(xi);
     Npp = 0;
     if j < 3
         Npp = 0;
     else
-        Npp = 6*((max(x(i)-xi(j-2),0)-max(x(i)-xi(5),0)))/(xi(5)-xi(j-2));
-        Npp = Npp - 6*((max(x(i)-xi(4),0)-max(x(i)-xi(5),0)))/(xi(5)-xi(4));
+        Npp = 6*((max(x(i)-xi(j-2),0)-max(x(i)-xi(num_knots),0)))/(xi(num_knots)-xi(j-2));
+        Npp = Npp - 6*((max(x(i)-xi(num_knots - 1),0)-max(x(i)-xi(num_knots),0)))/(xi(num_knots)-xi(num_knots - 1));
     end
 end
 
 % Calculate omega matrix
 function omega = calc_omega(N,x,xi)
-    omega = zeros(5,5);
+    num_knots = length(xi);
+    omega = zeros(num_knots, num_knots);
     step = 0.005;
-    for i=3:5
-        for j=3:5
+    for i=3:num_knots
+        for j=3:num_knots
             for k=1:N
                 Nppi = calc_Npp(k,i,x,xi);
                 Nppj = calc_Npp(k,j,x,xi);
